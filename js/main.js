c94474467ae7aaf27f35d54e0b5a16e2b12c994b
Shahzod <87805232+shahzodmaxammatov288@users.noreply.github.com>
2021-07-22 15:24:58 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function menuleft(klass) {
+    document.getElementById('menu-left').style.display=klass;
+}
+
+function almash1() {
+    document.getElementById('bars').style.display="none";
+    document.getElementById('remove').style.display="block";
+}
+function almash2() {
+    document.getElementById('remove').style.display="none";
+    document.getElementById('bars').style.display="block";
+}
+
